9b6e8b2897ff845861731c1617c58ffe0790c2a7
Esteban Carrillo <estexav0996@gmail.com>
2024-11-14 15:41:51 +0000

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -8,13 +8,22 @@ const absoluteAssetsPath = __dirname + "/public";
 express.static(absoluteAssetsPath);
 
 app.use("/public", express.static(absoluteAssetsPath))
+
+
+app.use(function (req, res, next) {
+    const { ip, method, path } = req
+    console.log(`${method} ${path} - ${ip}`);
+    next();
+
+})
+
 app.get("/", function (req, res) {
     res.sendFile(path)
 })
 app.get("/json", function (req, res) {
     let message = "Hello json";
-    if(process.env.MESSAGE_STYLE === "uppercase") message = message.toUpperCase(); 
-    res.json({ message: message})
+    if (process.env.MESSAGE_STYLE === "uppercase") message = message.toUpperCase();
+    res.json({ message: message })
 })
 
 
