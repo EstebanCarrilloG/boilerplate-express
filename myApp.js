24ce976e2b066603987edfe7be11ea1e6c6c99b3
Esteban Carrillo <estexav0996@gmail.com>
2024-11-14 14:45:16 +0000

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
+const path = __dirname + '/views/index.html'
 
-app.get("/", function(req,res){
-    res.send( "Hello Express")
+app.get("/", function (req, res) {
+    res.sendFile(path)
 })
 
 
@@ -41,4 +41,4 @@ app.get("/", function(req,res){
 
 
 
- module.exports = app;
+module.exports = app;
