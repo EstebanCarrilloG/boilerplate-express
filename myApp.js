34e884a799d5e4c25d2c9a41f5db914e2c32d80b
Esteban Carrillo <estexav0996@gmail.com>
2024-11-14 22:27:34 +0000

    Get Query Parameter Input from the Client, Get Data from POST Requests

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const bodyParser = require("body-parser")
 let express = require('express');
 let app = express();
 
@@ -8,7 +9,7 @@ const absoluteAssetsPath = __dirname + "/public";
 express.static(absoluteAssetsPath);
 
 app.use("/public", express.static(absoluteAssetsPath))
-
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(function (req, res, next) {
     const { ip, method, path } = req
@@ -32,8 +33,18 @@ app.get("/now", function (req, res, next) {
     res.send({ time: req.time });
 })
 // Get Route Parameter Input from the Client
-app.get("/:word/echo",function(req,res){
-    res.send({echo: req.params.word})
+app.get("/:word/echo", function (req, res) {
+    res.send({ echo: req.params.word })
+})
+// Get Query Parameter Input from the Client
+app.route("/name").get((req, res) => {
+    const { first, last } = req.query
+    const name = first + " " + last
+    res.json({ name: name })
+}).post((req, res) => {
+    const{first,last} = req.body;
+    const name = first + " " + last
+    res.json({name:name})
 })
 
 
