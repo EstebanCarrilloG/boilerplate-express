8bf7f7f3f5bdf787e7b8d24b85f7b31d48aa7772
Esteban Carrillo <estexav0996@gmail.com>
2024-11-14 15:02:14 +0000

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let app = express();
 
-const path = __dirname + '/views/index.html'
+const path = __dirname + '/views/index.html';
+const absoluteAssetsPath = __dirname + "/public";
 
+express.static(absoluteAssetsPath);
+
+app.use("/public", express.static(absoluteAssetsPath))
 app.get("/", function (req, res) {
     res.sendFile(path)
 })
